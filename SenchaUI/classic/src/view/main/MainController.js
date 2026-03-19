2554f255b33f7e9ce0c9a904b54b6f7acac55b30
JohnKamau79 <memiakamau79@gmail.com>
2026-03-19 17:23:13 +0300

    Added confirm delete popup

--- SenchaUI/classic/src/view/main/MainController.js
+++ SenchaUI/classic/src/view/main/MainController.js
@@ -1,37 +1,42 @@
-Ext.define('MyApp.view.main.MainController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.main',
+Ext.define("MyApp.view.main.MainController", {
+  extend: "Ext.app.ViewController",
+  alias: "controller.main",
 
-    onCreatePersonnel: function() {
-        Ext.create('MyApp.view.main.PersonnelFormWindow').show();
-    },
+  onCreatePersonnel: function () {
+    Ext.create("MyApp.view.main.PersonnelFormWindow").show();
+  },
 
-    onSavePopupPersonnel: function(btn) {
-        var form = btn.up('form').getForm();
-        if (!form.isValid()) return;
+  onSavePopupPersonnel: function (btn) {
+    var form = btn.up("form").getForm();
+    if (!form.isValid()) return;
 
-        var values = form.getValues();
-        var store = this.lookupReference('personnelGrid').getStore();
+    var values = form.getValues();
+    var store = this.lookupReference("personnelGrid").getStore();
 
-        var newRecord = store.add(values)[0];
-        newRecord.save({
-            success: function() {
-                form.reset();
-                btn.up('window').close();
-            },
-            failure: function(rec, operation) {
-                Ext.Msg.alert('Error', 'Failed to save personnel.');
-            }
-        });
-    },
+    var newRecord = store.add(values)[0];
+    newRecord.save({
+      success: function () {
+        form.reset();
+        btn.up("window").close();
+      },
+      failure: function (rec, operation) {
+        Ext.Msg.alert("Error", "Failed to save personnel.");
+      },
+    });
+  },
 
-    onDeletePersonnel: function(grid, rowIndex) {
-        var store = grid.getStore();
-        var rec = store.getAt(rowIndex);
-        rec.erase({
-            failure: function() {
-                Ext.Msg.alert('Error', 'Failed to delete personnel.');
-            }
-        });
-    }
+  onDeletePersonnel: function (grid, rowIndex) {
+    Ext.Msg.confirm(
+      "Confirm Delete",
+      "Are you sure you want to delete this item?",
+      function (btn) {
+        if (btn === "yes") {
+          var store = grid.getStore();
+          var rec = store.getAt(rowIndex);
+          rec.erase();
+          Ext.Msg.alert("Deleted", "The item has been deleted.");
+        }
+      },
+    );
+  },
 });
